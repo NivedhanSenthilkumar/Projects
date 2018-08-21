8934cbb0e88a04944cd7d6180963ca8c2276b08a
Milony Mehta <mehta.mil@husky.neu.edu>
2018-08-21 01:05:37 -0400

    Pushing project Default Credit Card Clients

---
+++ Default_Credit_Card_Clients/script2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+python models.py AKIAI6BF5SDIVG5M3FIA V9V+aoJE5KiyFVmdQNSaDvby3FqrveyalYo7Nvqy
+cd assignment3_Part2
+python app.py AKIAI6BF5SDIVG5M3FIA V9V+aoJE5KiyFVmdQNSaDvby3FqrveyalYo7Nvqy
+
