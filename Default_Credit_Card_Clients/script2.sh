102dd7bbdd19877c51e6ec3ace0ab5413b84bcf7
Milony Mehta <mehta.mil@northeastern.edu>
2018-08-21 01:13:37 -0400

    Delete script2.sh

--- Default_Credit_Card_Clients/script2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python models.py AKIAI6BF5SDIVG5M3FIA V9V+aoJE5KiyFVmdQNSaDvby3FqrveyalYo7Nvqy
-cd assignment3_Part2
-python app.py AKIAI6BF5SDIVG5M3FIA V9V+aoJE5KiyFVmdQNSaDvby3FqrveyalYo7Nvqy
-
